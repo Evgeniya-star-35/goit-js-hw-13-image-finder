33507b19803348626342aeff667abca502f87514
Evgeniya Bayda <baje75128@gmail.com>
2021-08-11 21:05:41 +0300

    add spinner

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import NewsApiService from './js/apiService';
 import refs from './js/refs';
 import markupImgTPL from './templates/markup-card.hbs';
+import LoadMoreBtn from './js/loadMoreBtn';
 import { error } from '@pnotify/core';
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/desktop/dist/PNotifyDesktop';
@@ -8,33 +9,38 @@ import '@pnotify/core/dist/BrightTheme.css';
 var debounce = require('lodash.debounce');
 import 'material-icons/iconfont/material-icons.css';
 
-refs.searchInput.addEventListener('input', debounce(onSearchImages, 500));
-refs.btn.addEventListener('click', onLoadMore);
-
 const newsApiService = new NewsApiService();
+const loadMoreBtn = new LoadMoreBtn({
+  selector: '[data-action="load-more"]',
+  hidden: true,
+});
+
+refs.searchInput.addEventListener('input', debounce(onSearchImages, 500));
+loadMoreBtn.refs.button.addEventListener('click', fetchImg);
 
 function onSearchImages(e) {
   e.preventDefault();
   newsApiService.query = e.target.value;
+  loadMoreBtn.show();
   newsApiService.resetPage();
-  newsApiService
-    .fetchImages()
-    .then(hits => {
-      clearImgGallery();
-      renderImgCard(hits);
-    })
-    .catch(onFetchError);
+  clearImgGallery();
+  fetchImg();
 }
 function renderImgCard(hits) {
   const markup = markupImgTPL(hits);
   refs.gallery.innerHTML = markup;
 }
-function onFetchError(Error) {
-  console.log(Error);
+function fetchImg() {
+  loadMoreBtn.disable();
+  newsApiService.fetchImages().then(hits => {
+    renderImgCard(hits);
+    loadMoreBtn.enable();
+  });
 }
-function onLoadMore() {
-  newsApiService.fetchImages().then(renderImgCard);
-}
+// function onFetchError(Error) {
+//   console.log(Error);
+// }
+
 function clearImgGallery() {
   refs.gallery.innerHTML = '';
   // newsApiService.emptyPage();
