d5036a4ea77e0e4ecc9a614ab03cf1b44f8b7428
Evgeniya Bayda <baje75128@gmail.com>
2021-09-03 21:41:56 +0300

    add button scroll

--- src/js/refs.js
+++ src/js/refs.js
@@ -4,8 +4,8 @@ const refs = {
   modalImg: document.querySelector('.lightbox__image'),
   modal: document.querySelector('.js-lightbox'),
   buttonModal: document.querySelector('.lightbox__button'),
-  arrowRightModal: document.querySelector('.lightbox__button--arrowRight'),
-  arrowLeftModal: document.querySelector('.lightbox__button--arrowLeft'),
+  // arrowRightModal: document.querySelector('.lightbox__button--arrowRight'),
+  // arrowLeftModal: document.querySelector('.lightbox__button--arrowLeft'),
   goTopBtn: document.querySelector('.back_to_top'),
 };
 export default refs;
